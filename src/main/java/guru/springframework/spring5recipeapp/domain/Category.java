b53febd43fa0ea8a465b00586a35db549a60e448
Mustafa Sahin <mustafashn14@gmail.com>
2021-10-25 13:18:45 +0300

    add Category Entity and relationship between Category and Recipe as many to many

---
+++ src/main/java/guru/springframework/spring5recipeapp/domain/Category.java
@@ -0,0 +1,49 @@
+package guru.springframework.spring5recipeapp.domain;
+
+import javax.persistence.*;
+import java.util.Set;
+
+/**
+ * Created by M.Şahin on 25/10/2021
+ */
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Recipe> recipes;
+
+    public Long getId()
+    {
+        return id;
+    }
+
+    public void setId(Long id)
+    {
+        this.id = id;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+
+    public Set<Recipe> getRecipes()
+    {
+        return recipes;
+    }
+
+    public void setRecipes(Set<Recipe> recipes)
+    {
+        this.recipes = recipes;
+    }
+}
