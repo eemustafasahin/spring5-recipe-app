373380a9143e7e6dfe3e23a7af54944c04a482f1
Mustafa Sahin <mustafashn14@gmail.com>
2021-10-25 12:41:39 +0300

    add enum type - Difficulty

---
+++ src/main/java/guru/springframework/spring5recipeapp/domain/Difficulty.java
@@ -0,0 +1,9 @@
+package guru.springframework.spring5recipeapp.domain;
+
+/**
+ * Created by M.Şahin on 25/10/2021
+ */
+public enum Difficulty {
+    EASY, MODERATE, HARD;
+
+}
--- src/main/java/guru/springframework/spring5recipeapp/domain/Recipe.java
+++ src/main/java/guru/springframework/spring5recipeapp/domain/Recipe.java
@@ -21,11 +21,12 @@ public class Recipe {
     private String source;
     private String url;
     private String direction;
-    //TODO
-    //private Difficulty difficulty;
 
     @Lob
     private Byte [] image;
+
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
 
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
@@ -131,5 +132,25 @@ public class Recipe {
     public void setNotes(Notes notes)
     {
         this.notes = notes;
+    }
+
+    public Difficulty getDifficulty()
+    {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty)
+    {
+        this.difficulty = difficulty;
+    }
+
+    public Set<Ingredient> getIngredients()
+    {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients)
+    {
+        this.ingredients = ingredients;
     }
 }
