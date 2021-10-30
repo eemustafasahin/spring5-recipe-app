8d073d8de1d2a8393bb32bc56d3c4be412dc55f7
Mustafa Sahin <mustafashn14@gmail.com>
2021-10-30 12:39:12 +0300

    add arguement captor to pick set of recipes in unit test properly.

--- src/test/java/guru/springframework/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/controllers/IndexControllerTest.java
@@ -1,17 +1,21 @@
 package guru.springframework.controllers;
 
+import guru.springframework.domain.Recipe;
 import guru.springframework.services.RecipeService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 /**
  * Created by M.Şahin on 30/10/2021
@@ -39,10 +43,23 @@ class IndexControllerTest {
     @Test
     void getIndexPage()
     {
+        //given
+        Set<Recipe> recipes = new HashSet<>();
+        recipes.add(new Recipe());
+        recipes.add(new Recipe());
+
+        when(m_recipeService.getRecipes()).thenReturn(recipes);
+
+        @SuppressWarnings(value = "unchecked")
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
         String viewName = m_controller.getIndexPage(m_model);
         assertEquals("index", viewName);
         verify(m_recipeService, times(1)).getRecipes();
-        verify(m_model, times(1)).addAttribute(eq("recipes"),anySet());
+        verify(m_model, times(1)).addAttribute(eq("recipes"),argumentCaptor.capture());
+
+        Set<Recipe> setInController = argumentCaptor.getValue();
+        assertEquals(1, setInController.size());
 
     }
 }
